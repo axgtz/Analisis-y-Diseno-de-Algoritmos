b765e671b25da38b899a7fb2abc1c019ae6fbfc4
axgtz <axguti@gmail.com>
2017-08-28 10:17:55 -0500

    bricksort

--- Algoritmos-de-Ordenamiento/Algoritmos-de-ordenamiento/main.cpp
+++ Algoritmos-de-Ordenamiento/Algoritmos-de-ordenamiento/main.cpp
@@ -1,15 +1,81 @@
-//
-//  main.cpp
 //  Algoritmos-de-ordenamiento
 //
 //  Created by Alex Gutz on 8/27/17.
 //  Copyright © 2017 Alejandro Gutierrez. All rights reserved.
-//
-
 #include <iostream>
 
+using namespace std;
+/*
+YA:
+ Print
+ 
+FALTAN:
+GLOBAL VARIABLES
+SWAP
+MANERA DE CALC EL TIEMPO
+RANDOMIZER
+ 
+Ordenamiento de burbuja 	(Bubble Sort)
+Ordenamiento de burbuja bidireccional (Cocktail Sort)
+Ordenamiento por inserción (Insertion Sort)
+Ordenamiento por casilleros (Bucket Sort)
+Ordenamiento por cuentas (Counting Sort)
+Ordenamiento por mezcla (Merge Sort)
+Ordenamiento con árbol binario(Binary tree Sort)
+Ordenamiento Radix (Radix Sort)
+Ordenamiento Shell (Shell Sort)
+Ordenamiento por selección (Selection Sort)
+Ordenamiento por montículos (Heap Sort)
+Ordenamiento rápido (Quick Sort)
+*/
+
+void CocktailSort(int a[], int n){
+    bool swapped = true;
+    int start = 0;
+    int end = n-1;
+    
+    while (swapped){
+        swapped = false;
+        
+        for (int i = start; i < end; ++i){
+            if (a[i] > a[i + 1]){
+                swap(a[i], a[i+1]);
+                swapped = true;
+            }
+        }
+        
+        if (!swapped)
+            break;
+        
+        swapped = false;
+        
+        --end;
+        
+        for (int i = end - 1; i >= start; --i){
+            if (a[i] > a[i + 1]){
+                swap(a[i], a[i+1]);
+                swapped = true;
+            }
+        }
+        ++start;
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    for (int i=0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int arr[] = {5, 1, 4, 2, 8, 0, 2};
+    int n = sizeof(arr)/ sizeof(arr[0]);
+    
+    CocktailSort(arr,n);
+    
+    printf("Sorted array :\n");
+    printArray(arr,n);
+    
     return 0;
 }
---
+++ BrickSort/BrickSort/main.cpp
@@ -0,0 +1,64 @@
+//  main.cpp
+//  BrickSort
+//
+//  Created by Alex Gutz on 8/28/17.
+//  Copyright © 2017 Alejandro Gutierrez. All rights reserved.
+#include <iostream>
+
+using namespace std;
+
+// A utility function ot print an array of size n
+void printArray(int arr[], int n)
+{
+    for (int i=0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+
+
+// A function to sort the algorithm using Odd Even sort
+void oddEvenSort(int arr[], int n)
+{
+    bool isSorted = false; // Initially array is unsorted
+    while (!isSorted)
+    {
+        isSorted = true;
+        
+        // Perform Bubble sort on ODD indexed element
+        for (int i=1; i<n-1; i=i+2)
+        {
+            if (arr[i] > arr[i+1])
+            {
+                printArray(arr, n);
+                swap(arr[i], arr[i+1]);
+                isSorted = false;
+            }
+        }
+        
+        // Perform Bubble sort on EVEN indexed element
+        for (int i=0; i<n-1; i=i+2)
+        {
+            if (arr[i] > arr[i+1])
+            {
+                printArray(arr, n);
+                swap(arr[i], arr[i+1]);
+                isSorted = false;
+            }
+        }
+    }
+    
+    return;
+}
+
+
+// Driver program to test above functions.
+int main()
+{
+    int arr[] = {1,9,3,10,15,18,14,13,8,2,7,11,6,19,4,17,5,12,20,16};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    oddEvenSort(arr, n);
+    printArray(arr, n);
+    
+    return (0);
+}
