4916ad30103ada4905523c032a34e0fa99edf20a
axgtz <axguti@gmail.com>
2017-10-02 23:57:38 -0500

    idk num 2

--- Tarea-3-Disenio-Alg/2-Parrafo/2-Parrafo/main.cpp
+++ Tarea-3-Disenio-Alg/2-Parrafo/2-Parrafo/main.cpp
@@ -7,11 +7,57 @@ desaparecen. El costo de fijar la línea pi, pi+1, ..., pj es (j – i)|b’ – b|, si
 decir (L – li – li+1 – ... – lj)/(j – i). No obstante, si j = n (la última palabra) el costo será cero a menos que b’ < b 
 (ya que no es necesario ampliar la última línea).*/
 #include <iostream>
+#include <vector>
 using namespace std;
 //Tecnica: Algoritmo Avido
 //Complejidad O(n)
 int main() {
+	vector<string> parrafo, linea;
+	vector<float> l;
+	float L = 20, b = 1, length = 0, linelength = 0, extra, expanding, reducing;
+	int i = 0, j;
+	bool imprimio = true;
 
+
+	for (j = 0; j < parrafo.size(); j++){
+		if (imprimio){
+			i = j;
+			printed = false;
+			linea.clear();
+			length = 0;
+		}
+		length += l[j];
+		linelength = length + ((j - i) * b);
+		if (linelength > L){
+			extra = linelength - L;
+			reducing = b - (extra / (j - i));
+			expanding = b + (L - ((length - l[j]) + (b * (j - i - 1)))) / (j - i - 1);
+
+			if (((fabs(expanding - b) * (j - i - 1)) > (fabs(reducing - b) * (j - i))) && reducing > 0){
+				linea.push_back(parrafo[j]);
+				for (auto x : linea) {
+					cout << x << " ";
+				}
+				cout << reducing << "r";
+				cout << "\n";
+				imprimio = true;
+
+			}else{
+				for (auto x : linea){
+					cout << x << " ";
+				}
+				cout << expanding << "a";
+				cout << "\n";
+				j--;
+				imprimio = true;
+			}
+		}
+		linea.push_back(parrafo[j]);
+	}
+	for (auto x : linea){
+		cout << x << " ";
+	}
+	cout << "\n";
 
 	return 0;
 }
