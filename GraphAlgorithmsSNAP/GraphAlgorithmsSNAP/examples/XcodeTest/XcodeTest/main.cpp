f30e00d9cc2e6797f68584db6f2c7c840866e0c5
axgtz <axguti@gmail.com>
2017-10-16 00:26:41 -0500

    mercyy

--- GraphAlgorithmsSNAP/GraphAlgorithmsSNAP/examples/XcodeTest/XcodeTest/main.cpp
+++ GraphAlgorithmsSNAP/GraphAlgorithmsSNAP/examples/XcodeTest/XcodeTest/main.cpp
@@ -3,17 +3,31 @@
 //  XcodeTest
 
 #include "/Users/gutz/Downloads/Snap-4.0/snap-core/Snap.h"
+
+#include "stdafx.h"
+
 #include <iostream>
 #include <vector>
 #include <chrono>
 #include <limits>
 #include <queue>
 
+#include "disjointset.h"
+
 using namespace TSnap;
+using namespace std;
+using namespace std::chrono;
 
 int main(int argc, char* argv[]) {
     
     TPt<TNodeEDatNet<TInt, TFlt> >  g = TNodeEDatNet<TInt, TFlt>::New();
+    high_resolution_clock::time_point t1 = high_resolution_clock::now();
+    insertVertex(g);
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    duration<double> time_span = duration_cast<duration<double> >(t2 - t1);
+    cout << "Tiempo de agregar vertice: " << time_span.count()  << "seconds" << endl;
+    insertVertex(g);
+    
     
     //Add vertices
     for (int i = 1; i < 15; i++) {
@@ -44,5 +58,43 @@ int main(int argc, char* argv[]) {
     g->AddEdge(12,14,9);
     g->AddEdge(13,14,6);
     g->AddEdge(14,13,2);
+    
+    
+    t1 = high_resolution_clock::now();
+    prim(g);
+    t2 = high_resolution_clock::now();
+    time_span = duration_cast<duration<double> >(t2 - t1);
+    cout << "Tiempo de Prim: " << time_span.count()  << "seconds" << endl;
+    
+    t1 = high_resolution_clock::now();
+    BFS(1);
+    t2 = high_resolution_clock::now();
+    time_span = duration_cast<duration<double> >(t2 - t1);
+    cout << "Tiempo de BFS: " << time_span.count()  << "seconds" << endl;
+    
+    t1 = high_resolution_clock::now();
+    dfsInit(1);
+    t2 = high_resolution_clock::now();
+    time_span = duration_cast<duration<double> >(t2 - t1);
+    cout << "Tiempo de DFS: " << time_span.count()  << "seconds" << endl;
+    
+    t1 = high_resolution_clock::now();
+    kruskals(g);
+    t2 = high_resolution_clock::now();
+    time_span = duration_cast<duration<double> >(t2 - t1);
+    cout << "Tiempo de Kruskal: " << time_span.count()  << "seconds" << endl;
+    
+    t1 = high_resolution_clock::now();
+    floydwarshall(g);
+    t2 = high_resolution_clock::now();
+    time_span = duration_cast<duration<double> >(t2 - t1);
+    cout << "Tiempo de Floyd Warshall: " << time_span.count()  << "seconds" << endl;
+    
+    
+    t1 = high_resolution_clock::now();
+    dijkstras(g,1);
+    t2 = high_resolution_clock::now();
+    time_span = duration_cast<duration<double> >(t2 - t1);
+    cout << "Tiempo de Dijkstras: " << time_span.count()  << "seconds" << endl;
   return 0;
 }
